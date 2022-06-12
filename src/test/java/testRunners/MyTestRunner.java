312852c4d85a2b77f82b5c490c3b77175c1bde50
shwetha <shwethamanohari@gmail.com>
2022-06-12 14:32:54 -0700

    my first commit

---
+++ src/test/java/testRunners/MyTestRunner.java
@@ -0,0 +1,22 @@
+package testRunners;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"src/test/resources/AppFeatures"},
+		glue = {"stepdefinations", "AppHooks"},
+		plugin = {"pretty",  
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+		monochrome= true,
+		 dryRun = false
+	
+		)
+
+
+
+
+public class MyTestRunner {
+
+}
