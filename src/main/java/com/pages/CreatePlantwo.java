ef15bb9fe324d01005e58a4a9860395586649a84
jpriyanga9 <105875439+jpriyanga9@users.noreply.github.com>
2022-06-16 13:45:54 -0400

    Update and rename CreatePlanTwo.java to CreatePlantwo.java
    commit CreatePlantwo

---
+++ src/main/java/com/pages/CreatePlantwo.java
@@ -0,0 +1,71 @@
+package com.pages;
+
+public class CreatePlantwo {
+  
+	  
+static WebDriver driver;
+	
+	//public By clickBrowse2 = By.xpath("") ;
+	//public By browseTag2 = By.xpath("") ;
+	//public By clickUpload2 = By.xpath("") ;
+	//public By uploadTag2 = By.xpath("") ;
+	//public By toggleBtn2 = By.xpath("") ;
+	//public By confirmBtn2 = By.xpath("") ;
+	//public By confirmTag2 = By.xpath("") ;
+	//public By GenerateBtn = By.xpath("") ;
+	//public By GenerateTag = By.xpath("") ;
+
+	
+	public CreatePlantwo(WebDriver driver) {
+			// TODO Auto-generated constructor stub
+		searchResults.driver=driver; 
+	}
+
+	public void clickBrowseOption2() {
+		//driver.findElement(clickBrowse2).click();
+		System.out.println("clickBrowseOption is successful");
+	}
+
+	public void browseTag2() {
+		//browseTag2.gettext();
+		System.out.println("Browse is successful");
+	}
+	
+	public void clickUploadOption2() {
+		//driver.findElement(clickBrowse2).click();
+		System.out.println("clickUploadOption is successful");
+	}
+	
+	public void UploadTag2() {
+		//uploadTag2.gettext();
+		System.out.println("UploadTag is successful");
+	}
+	
+	public void select2() {
+		//toggleBtn2.click();
+
+		System.out.println("select is successful");
+	}
+	
+	public void clickConfirm2() {
+		//driver.findElement(confirmBtn2).click();
+		System.out.println("confirmBtn is successful");
+	}
+	
+	public void confirmTag2() {
+		//confirmTag2.gettext();
+		System.out.println("confirmTag is successful");
+	}
+	
+	public void Generate() {
+		//driver.findElement(GenerateBtn).click();
+		System.out.println("Generate is successful");
+	}
+	
+	public void generateTag() {
+		//GenerateTag.gettext();
+		System.out.println("generateTag is successful");
+	}
+	
+
+}
